ed6fa5ce90a9eed0da8af7a0d566d486dfea1caf
JavaStream <radeon5@yandex.ru>
2019-04-09 23:55:57 +0300

    Migration from MySQL to PostgreSQL

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,37 +1,33 @@
+create sequence hibernate_sequence start 1 increment 1;
+
 create table client (
-    id integer not null,
-    description varchar(2048),
+    id int4 not null,
+    description varchar(255) not null,
     filename varchar(255),
     name varchar(255),
-    user_id bigint,
+    user_id int8,
     primary key (id)
 );
 
-create table hibernate_sequence (next_val bigint);
-
-insert into hibernate_sequence values ( 1 );
-
-insert into hibernate_sequence values ( 1 );
-
 create table user_role (
-    user_id bigint not null,
+    user_id int8 not null,
     roles varchar(255)
 );
 
 create table usr (
-    id bigint not null,
+    id int8 not null,
     activation_code varchar(255),
-    active bit not null,
+    active boolean not null,
     email varchar(255),
     password varchar(255) not null,
     username varchar(255) not null,
     primary key (id)
 );
 
-alter table client
+alter table if exists client
     add constraint client_user_fk
-    foreign key (user_id) references usr (id);
+    foreign key (user_id) references usr;
 
-alter table user_role
+alter table if exists user_role
     add constraint user_role_user_fk
-    foreign key (user_id) references usr (id);
+    foreign key (user_id) references usr;
--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -2,7 +2,4 @@ insert into usr (id, username, password, active)
     values (1, 'admin', 's', true);
 
 insert into user_role (user_id, roles)
-    values (1, 'USER');
-
-insert into user_role (user_id, roles)
-    values (1, 'ADMIN');
+    values (1, 'USER'), (1, 'ADMIN');
