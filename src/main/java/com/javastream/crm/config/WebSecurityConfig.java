2fa17865a7af56b3ffc8a3684e4de5444e98a232
javastream <radeon5@yandex.ru>
2019-04-25 08:47:22 +0300

    test commit

--- src/main/java/com/javastream/crm/config/WebSecurityConfig.java
+++ src/main/java/com/javastream/crm/config/WebSecurityConfig.java
@@ -2,13 +2,16 @@ package com.javastream.crm.config;
 
 import com.javastream.crm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // При старте приложения конфигурирует Security
 @Configuration
@@ -18,6 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,6 +50,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());             // шифрует пароли, чтобы они не хранились в явном виде
+                .passwordEncoder(passwordEncoder);             // шифрует пароли, чтобы они не хранились в явном виде
     }
 }
